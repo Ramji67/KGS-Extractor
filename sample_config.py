da979ea41a62e73508a00759308eca352791fa22
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2024-10-14 21:22:31 +0530

    sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 28228907
-api_hash = os.environ.get("API_HASH", "3848da16c807890cabc8d1395248c9cf3")
+api_id = 28088290
+api_hash = os.environ.get("API_HASH", "6998f2c585fdce65ac72dfa23d02b6ec")
 bot_token = os.environ.get("BOT_TOKEN")
-auth_users = os.environ.get("AUTH_USERS", "6127558921")
+auth_users = os.environ.get("AUTH_USERS", "5850397219")
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = os.environ.get("OWNER_USERS", "6127558921")
+osowner_users = os.environ.get("OWNER_USERS", "5850397219")
 owner_users = [int(num) for num in osowner_users.split(",")]
